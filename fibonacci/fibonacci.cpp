1f3635e7bf64561987bb872859a0e9555042cc39
Fanli Lin <fanli.lin@uni-jena.de>
2018-01-31 14:58:31 +0100

    invalid argument not allowed

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -8,6 +8,8 @@
 typedef std::vector<unsigned long> Vector;
 
 unsigned long fibonacci(unsigned n) {
+
+	if (n < 0) throw std::invalid_argument{"n must be non-negative"};
     if(n==0) return 0;
     if(n==1) return 1;
 
