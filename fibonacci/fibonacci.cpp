f84fc3843b682dd588d02aca11a597f13f3d164d
Fanli Lin <fanli.lin@uni-jena.de>
2018-01-31 16:07:38 +0100

    small change

--- fibonacci/fibonacci.cpp
+++ fibonacci/fibonacci.cpp
@@ -9,7 +9,7 @@ typedef std::vector<unsigned long> Vector;
 
 unsigned long fibonacci(unsigned n) {
 
-	if (n < 0) throw std::invalid_argument{"n must be non-negative"};
+
     if(n==0) return 0;
     if(n==1) return 1;
 
