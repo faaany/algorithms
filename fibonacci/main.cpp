80f65d0dbe100541f145839718262a6ff1a4497e
Fanli Lin <fanli.lin@uni-jena.de>
2018-01-31 13:45:56 +0100

    merging all 3 solutions.

--- fibonacci/main.cpp
+++ fibonacci/main.cpp
@@ -5,15 +5,15 @@ using std::cout;
 
 int main() {
 
-    cout<<"Fibonacci numbers recursive   linear  constant space\n";
+    cout<<"Fibonacci recursive   linear   constant space\n";
     for(unsigned n:{0,1,2,3,4,5,6,7,8,9,10}) {
 
         auto result_rec = fibonacci(n);
         auto result_lin = fibonacciLinear(n);
-        auto resultConst= fibonacciConst(n);
+        auto result_const= fibonacciConst(n);
 
-        cout << "f(" << n << ") = " << result_rec << "       " << result_lin <<"   "<<resultConst << "\n";
+        cout << "f(" << n << ") =           " << result_rec << "         " << result_lin <<"      "<<result_const << "\n";
     }
-    
+
     return 0;
 }
