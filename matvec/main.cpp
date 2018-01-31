7ed92d37066dc0c628a318a83d6a10bcf612a80c
Fanli Lin <fanli.lin@uni-jena.de>
2018-01-31 01:10:48 +0100

    reading parameters from command line

--- matvec/main.cpp
+++ matvec/main.cpp
@@ -3,44 +3,55 @@
 #include <vector>
 #include <random>
 
-using std::mt19937;
-using std::normal_distribution;
-using std::bind;
-using std::ostream;
+
 using std::cout;
+using std::cin;
+using namespace std::chrono;
 
 typedef long double extended;
 typedef std::vector<extended> Vector;
 
-mt19937 generator = mt19937(std::random_device()());
-normal_distribution<extended> normal;
-auto normalGenerator = bind(normal, generator);
+auto generator = std::mt19937(std::random_device()());
+auto normal= std::uniform_real_distribution<extended>(0,1);
+auto normalGenerator = std::bind(normal, generator);
 
 
-void matVec(Vector& mat, Vector& vec, Vector& result, const unsigned N);
+void matVec(const Vector& mat, const Vector& vec, Vector& result, unsigned N);
+void initOperands(Vector& mat, Vector& vec, unsigned N);
 
-void initMat(Vector& mat, Vector& vec, const unsigned N);
+int main(int nArgs, char** args) {
 
-int main() {
+    unsigned N=1000, k=200;
+    if(nArgs>1) N= unsigned(abs(atoi(args[1])));
+    if(nArgs>2) k= unsigned(abs(atoi(args[2])));
 
-    unsigned N, k;
+//    cout << "Please enter the matrix size N: ";
+//    cin >> N;
+//
+//    cout << "Please enter the number of iterations: ";
+//    cin >> k;
 
     Vector mat(N*N);
     Vector vec(N);
     Vector result(N);
 
-    initMat(mat, vec,N);
+    initOperands(mat, vec, N);
 
+    auto start = high_resolution_clock::now();
     for(unsigned i=0; i<k; ++i) {
         matVec(mat, vec, result,N);
     }
+    auto stop = high_resolution_clock::now();
 
+    extended duration = duration_cast<nanoseconds>(stop-start).count()*1e-9;
+
+    cout << "duration for N = " << N << " with "<< k << " repetations is " << duration << "s. \n";
 
     return 0;
 }
 
 
-void initMat(Vector& mat, Vector& vec, const unsigned N) {
+void initOperands(Vector& mat, Vector& vec, unsigned N) {
 
     for(unsigned i=0; i<N; ++i) {
         for(unsigned j=0; j<N; ++j) {
@@ -52,7 +63,7 @@ void initMat(Vector& mat, Vector& vec, const unsigned N) {
 }
 
 
-void matVec(Vector& mat, Vector& vec, Vector& result, const unsigned N) {
+void matVec(const Vector& mat, const Vector& vec, Vector& result, const unsigned N) {
 
     for(unsigned i=0; i<N; ++i) {
         extended sum =0;
